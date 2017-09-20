6470dda4e6321a949501fa5f54f0508a256ccb1d
Cuity <cty@commoditiesinchina.com>
2017-09-20 14:44:34 +0800

    projectInit

---
+++ controller/index.js
@@ -0,0 +1,6 @@
+/**
+ * IndexController
+ */
+exports.index = function(req, res) {
+    res.render('index');
+};
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+/**
+ * IndexRouter
+ */
+var Index = require('../controller/index.js');
+
+module.exports = function(app) {
+  // 首页
+  app.get('/', Index.index);
+};
