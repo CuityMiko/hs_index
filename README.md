b3266b81c9d6d3cb01dee366bd6c8e4e611b70af
崔同洋 <cuitongyang@chuangjiangx.com>
2018-04-24 07:09:15 +0800

    修改目录结构

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-### CSstroeFE个人主页
+### CJ_FE 基于node搭建的http-server，可提供api调用
 
-+ 1. api 分支主要提供API接口调用测试，配置可支持跨域
++ 1. 默认端口号为3003，可在config/site_conf文件下修改port字段
++ 2. 项目运行
+```bash
+
+# git clone
+git clone https://github.com/CuityMiko/hs_index.git
+or
+git clone -b api https://github.com/CuityMiko/hs_index.git
+
+# install
+cd hs_index
+npm install
+
+# install node-dev
+npm install node-dev -g
+
+# run
+npm start
+
+# browser
+http://localhost:3003/
+```
++ 3.
--- app.js
+++ app.js
@@ -5,21 +5,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-//加载ueditor 模块
-var ueditor = require("ueditor");
-//图片验证码
+// 图片验证码
 var svgCaptcha = require('svg-captcha');
+var expressLayouts = require('express-ejs-layouts'); // layout for html
 
 var app = express();
-// var mongoose=require('./config/mongoose.js');
-// var db=mongoose();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
-
+app.use(expressLayouts);
+// 指定全局的模板页面
+app.set('layout', '_layout/root')
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -28,14 +26,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-//这里周期只设置为20秒，为了方便测试
-//secret在正式用的时候务必修改
-//express中间件顺序要和下面一致
-
-app.use(session({//session持久化配置
+// 这里周期只设置为20秒，为了方便测试
+// secret在正式用的时候务必修改
+// express中间件顺序要和下面一致
+// session持久化配置
+app.use(session({
   secret: "cjnode",
   key: "cjnode",
-  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//超时时间
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},// 超时时间
   saveUninitialized: true,
   resave: false,
 }));
